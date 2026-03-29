cd0170ef30645bf25fa1fc716f435c9572ff8dc3
luis-c2255 <lmodesto63@gmail.com>
2026-03-29 04:47:58 +0200

    Add Spotify Wrapped 2025 analysis page

---
+++ pages/7_🎶_Spotify_Wrapped_2025.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from utils.theme import Components
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("🎶 :violet[Spotify Wrapped 2025 Analysis]", text_alignment="center")
