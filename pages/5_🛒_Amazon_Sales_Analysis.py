ae3e86369f088b57750efd8ca9efa1bd90c6f161
luis-c2255 <lmodesto63@gmail.com>
2026-03-29 04:45:24 +0200

    Add Amazon Sales Analysis page with Streamlit

---
+++ pages/5_🛒_Amazon_Sales_Analysis.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from utils.theme import Components
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("🛒 :orange[Amazon Sales Analysis Analysis]", text_alignment="center")
